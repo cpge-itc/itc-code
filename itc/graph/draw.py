2c14886fd17535f52ce4a9e1c3f813b837dc5e7d
Quentin Fortier <qpfortier@gmail.com>
2022-05-29 17:28:42 +0000

    Rm draw_weighted

--- itc/graph/draw.py
+++ itc/graph/draw.py
@@ -42,12 +42,3 @@ def draw_graph(G):
     G_.add_edges_from([(i, j) for i in range(len(G)) for j in G[i]])
     nx.draw_networkx(G_, font_color="w", node_color="black", node_size=600, arrowsize=35, font_size=16)
     plt.show()
-
-
-def draw_weighted_graph(G):
-    pos = spring_pos(G)
-    options = {"font_size": 20, "node_size": 700, "edgecolors": "black", "node_color": "white"}
-    nx.draw(G, pos, with_labels=True, **options)
-    labels = nx.get_edge_attributes(G, "weight")
-    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=labels)
-    plt.show()
